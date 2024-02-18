22858e3928bc4c31ca8e441db3c15276a4506ef1
chenc <chenc@XPS870>
2024-02-17 19:38:19 -0800

    add file check update site icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Convert .hol to .ics';
+  }, []);
+
   const [file, setFile] = useState(null);
   const [filename, setFilename] = useState(null);
   const [uploadComplete, setUploadComplete] = useState(false);
@@ -12,6 +16,22 @@ function App() {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
+
+    // check if file is selected
+    if (!file) {
+      alert('Please select a file');
+      return;
+    }
+    // check if file is .hol
+    if (!file.name.endsWith('.hol')) {
+      alert('Please select a .hol file');
+      return;
+    }
+    // check if file is less than 1MB
+    if (file.size > 1000000) {
+      alert('File size must be less than 1MB');
+      return;
+    }
 
     const formData = new FormData();
     formData.append('file', file);
@@ -42,6 +62,10 @@ function App() {
 
   return (
     <div className="App">
+      <div className="description-card">
+        <h2>Welcome</h2>
+        <p>Convert .hol file to ics file</p>
+      </div>
       <h2 className="title">Upload .hol File</h2>
       <form onSubmit={onFormSubmit} className="upload-form">
         <input type="file" onChange={onFileChange} className="file-input" />
